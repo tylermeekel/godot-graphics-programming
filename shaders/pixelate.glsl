04cad0842852ff9dff1e8a3bf4538577f284465f
Tyler Meekel <ty.meekel@gmail.com>
2025-07-20 20:42:24 -0600

    add pixelate filter

---
+++ shaders/pixelate.glsl
@@ -0,0 +1,31 @@
+#[compute]
+#version 450
+
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+layout(rgba16f, set = 0, binding = 0) uniform image2D color_image;
+
+layout(push_constant, std430) uniform Params {
+    vec2 raster_size;
+    vec2 reserved;
+} params;
+
+void main() {
+    ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
+    ivec2 size = ivec2(params.raster_size);
+
+    if (uv.x >= size.x || uv.y >= size.y) {
+        return;
+    }
+
+    vec4 color = imageLoad(color_image, uv);
+
+    if (color.a == 0) {
+        return;
+    } else {
+        float pixel_size = 10.0;
+
+        ivec2 pixel_uv = ivec2(floor(uv / pixel_size) * pixel_size);
+        vec4 pixel_color = imageLoad(color_image, pixel_uv);
+        imageStore(color_image, uv, pixel_color);
+    }
+}
